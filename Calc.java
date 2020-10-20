d9e8bae70c907998cfb02c3a0e2e3eb1bbbe63e3
rohan1229 <72907588+rohan1229@users.noreply.github.com>
2020-10-20 11:17:49 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -8,8 +8,13 @@ class Calc{
     return a-b;
   }
   
+  public static int mul(int a, int b){
+    return a*b;
+  }
+  
   public static void main(String args[]){
     System.out.println(add(10, 20));
     System.out.println(sub(60, 20));
+    System.out.println(mul(60, 20));
   }
 }
